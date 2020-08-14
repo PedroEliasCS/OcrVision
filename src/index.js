0b030825a922aaf29fd1dbeaeef02d406f104946
PedroEliasCS <peedro_eliias@hotmail.com>
2020-08-14 16:21:57 -0300

    pedro

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('133')
+const json1 = require('./a.json')
+
+
+for(let prop in json1['responses'][0]['textAnnotations']){
+    console.log(json1['responses'][0]['textAnnotations'][prop]['description'])
+}
